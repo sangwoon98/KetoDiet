b3b594ff0829fc9ff130770c0762067664f2d904
JauntySoul <joosunkim99@icloud.com>
2023-03-30 19:21:13 +0900

    cleaning code

--- ketodiet_flutter_project/lib/main.dart
+++ ketodiet_flutter_project/lib/main.dart
@@ -3,12 +3,12 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
 import 'package:url_strategy/url_strategy.dart';
 
-import 'test.dart';
-import 'pages/sign_page.dart';
+import 'pages/main_page.dart';
+import 'pages/test_page.dart';
 
 final routes = {
-  '/': (BuildContext context) => const HomePage(),
-  '/test': (BuildContext context) => const TestPage()
+  '/': (BuildContext context) => const MainPage(),
+  '/test': (BuildContext context) => const TestPage(),
 };
 
 void main() async {
@@ -37,46 +37,5 @@ class MyApp extends StatelessWidget {
       ),
       routes: routes,
     );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            SignInButton(),
-            SizedBox(
-              height: 50.0,
-            ),
-            TestGround(),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class TestPage extends StatefulWidget {
-  const TestPage({super.key});
-
-  @override
-  State<TestPage> createState() => _TestPageState();
-}
-
-class _TestPageState extends State<TestPage> {
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
   }
 }
