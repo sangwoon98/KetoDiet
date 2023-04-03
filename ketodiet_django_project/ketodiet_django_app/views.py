476876f96a0efee170f67a005276ceed46ea6ef4
sangwoon98 <dbstkddns123@naver.com>
2023-04-03 15:32:22 +0900

    04/03 15:32

--- ketodiet_django_project/ketodiet_django_app/views.py
+++ ketodiet_django_project/ketodiet_django_app/views.py
@@ -10,29 +10,19 @@ import jwt
 # from rest_framework.views import APIView
 #_______________________________________
 
+def access_token_validation(access_token): # 액세스 토큰 유효성 검사 함수
+    headers = {
+        'Authorization': f'Bearer {access_token}',
+    }
+    url = 'https://kapi.kakao.com/v1/user/access_token_info'
+    response = requests.get(url, headers=headers)
+    return response
+    
+
 @api_view(['GET'])
 def account(request):
-    access_token = request.GET.get('access_token', None)
-    expires_at = request.GET.get('expires_at', None)
-    refresh_token = request.GET.get('refresh_token', None)
-    refresh_token_expires_at = request.GET.get('refresh_token_expires_at', None)
-    scopes = request.GET.get('scopes', None)
-    id_token = request.GET.get('id_token', None)
+    access_token = request.headers.get('Authorization').split()[1] 
     print('access_token: ',access_token)
-    print('expires_at: ', expires_at)
-    print('refresh_token: ', refresh_token)
-    print('refresh_token_expires_at: ',refresh_token_expires_at)
-    print('scopes: ',scopes)
-    print('id_token: ',id_token)
-    
-    
-    def access_token_validation(access_token): # 액세스 토큰 유효성 검사
-        headers = {
-            'Authorization': f'Bearer {access_token}',
-        }
-        url = 'https://kapi.kakao.com/v1/user/access_token_info'
-        response = requests.get(url, headers=headers)
-        return response
     
     response = access_token_validation(access_token) # 액세스 토큰 유효성 검사
     if response.status_code == status.HTTP_200_OK:
@@ -57,26 +47,39 @@ def account(request):
 
 
 
-    # decoded_token = jwt.decode(access_token, verify=False)
+    # # decoded_token = jwt.decode(access_token, verify=False)
 
-    # print(decoded_token)
-    # id_token을 .으로 구분하여 헤더, 페이로드, 서명으로 분리합니다.
-    header_b64, payload_b64, signature_b64 = id_token.split('.')
+    # # print(decoded_token)
+    # # id_token을 .으로 구분하여 헤더, 페이로드, 서명으로 분리합니다.
+    # header_b64, payload_b64, signature_b64 = id_token.split('.')
 
-    # 헤더와 페이로드를 디코딩합니다.
-    header = json.loads(base64.b64decode(header_b64).decode('utf-8'))
-    payload = json.loads(base64.b64decode(payload_b64).decode('utf-8'))
+    # # 헤더와 페이로드를 디코딩합니다.
+    # header = json.loads(base64.b64decode(header_b64).decode('utf-8'))
+    # payload = json.loads(base64.b64decode(payload_b64).decode('utf-8'))
 
-    # 결과를 출력합니다.
-    print('Header:', header)
-    print('Payload:', payload)
-    print('Signature:', signature_b64)
-    print('access_token: ',access_token)
+    # # 결과를 출력합니다.
+    # print('Header:', header)
+    # print('Payload:', payload)
+    # print('Signature:', signature_b64)
+    # print('access_token: ',access_token)
     
-    if access_token:
-        return Response({'scopes': scopes})
-    else:
-        return Response({'scopes': 'None'}, status=400)
+    # if access_token:
+    #     return Response({'scopes': scopes})
+    # else:
+    #     return Response({'scopes': 'None'}, status=400)
+    
+    # access_token = request.GET.get('access_token', None)
+    # expires_at = request.GET.get('expires_at', None)
+    # refresh_token = request.GET.get('refresh_token', None)
+    # refresh_token_expires_at = request.GET.get('refresh_token_expires_at', None)
+    # scopes = request.GET.get('scopes', None)
+    # id_token = request.GET.get('id_token', None)
+    # print('access_token: ',access_token)
+    # print('expires_at: ', expires_at)
+    # print('refresh_token: ', refresh_token)
+    # print('refresh_token_expires_at: ',refresh_token_expires_at)
+    # print('scopes: ',scopes)
+    # print('id_token: ',id_token)
 
 
 # # views.py
