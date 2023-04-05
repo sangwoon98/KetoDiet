1005e3d80abf5b838c65c8370b5ced4510a8fa5c
JauntySoul <joosunkim99@icloud.com>
2023-04-05 09:31:06 +0900

    2023/04/05 09:30

--- ketodiet_flutter_project/lib/pages/sign_page.dart
+++ ketodiet_flutter_project/lib/pages/sign_page.dart
@@ -28,7 +28,7 @@ class _SignPageState extends State<SignPage> {
         builder: (context, snapshot) {
           if (snapshot.hasData) {
             if (snapshot.data == 'register') {
-              return registerWidget(context);
+              return RegisterWidget().form(context);
             } else if (snapshot.data == true) {
               WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
                 Navigator.pop(context);
@@ -36,7 +36,9 @@ class _SignPageState extends State<SignPage> {
             }
           }
 
-          return const SizedBox();
+          return Container(
+            color: Colors.green,
+          );
           // TODO: Sign Processing 창 만들기
         },
       ),
@@ -100,71 +102,154 @@ Future<dynamic> signIn(context) async {
 }
 
 Future<bool> signOut(context) async {
-  dropToken(context);
+  await dropToken(context);
   return true;
 }
 
-Widget registerWidget(context) {
-  TextEditingController controller = TextEditingController();
+class RegisterWidget {
+  TextEditingController registerController = TextEditingController();
 
-  // TODO: 회원가입 창
-  return Center(
-    child: SizedBox(
-      width: 300.0,
-      height: 300.0,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text('좋은 말로 할때 닉네임 입력하쇼.\n이건 내 마지막 경고요.'),
-          TextField(
-            controller: controller,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                onPressed: () async {
-                  await dropToken(context);
-                  WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
-                    Navigator.pop(context);
-                  });
-                },
-                child: const Text('취소'),
-              ),
-              ElevatedButton(
-                onPressed: () async {
-                  OAuthToken? token = await getToken(context);
-                  http.Response response = await http.post(
-                    Uri.http(backendDomain, '/api/account'),
-                    headers: {
-                      'Content-Type': 'application/json',
-                      'Accept': 'application/json',
-                      'Authorization': '${token!.toJson()}',
-                    },
-                    body: jsonEncode({
-                      'name': controller.text,
-                    }),
-                  );
-
-                  if (response.statusCode == 200) {
-                    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
-                      Navigator.pop(context);
-                    });
-                  } else {
-                    handleError(
-                      context,
-                      'Response Status Code Error.\nStatusCode: ${response.statusCode}',
-                      'sign_page.dart',
-                      'registerWidget',
-                    );
-                  }
-                },
-                child: const Text('확인'),
-              ),
-            ],
-          ),
-        ],
+  Widget form(context) {
+    return Center(
+      child: Card(
+        elevation: 20.0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20.0),
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            padding(welcomeText(), 10, 10, 10, 10),
+            padding(explainText(), 10, 10, 10, 10),
+            padding(textField(), 10, 10, 10, 10),
+            padding(buttons(context), 0, 10, 0, 0),
+          ],
+        ),
       ),
-    ),
-  );
+    );
+  }
+
+  Widget padding(widget, left, top, right, bottom) {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(left, top, right, bottom),
+      child: widget,
+    );
+  }
+
+  Widget welcomeText() {
+    return const Text(
+      '환영합니다!',
+      style: TextStyle(
+        color: Colors.green,
+        fontSize: 60.0,
+        fontWeight: FontWeight.bold,
+      ),
+    );
+  }
+
+  Widget explainText() {
+    return const Text(
+      'KetoDiet에 처음 방문하셨네요\n닉네임을 설정해주세요',
+      style: TextStyle(
+        fontSize: 20.0,
+      ),
+      textAlign: TextAlign.center,
+    );
+  }
+
+  Widget textField() {
+    // TODO: textField 수정
+    return SizedBox(
+      width: 300.0,
+      child: TextField(
+        controller: registerController,
+        decoration: const InputDecoration(
+          labelText: 'labelText',
+          helperText: 'helperText',
+          hintText: 'hintText',
+          errorText: 'errorText',
+          border: OutlineInputBorder(),
+        ),
+      ),
+    );
+  }
+
+  Widget buttons(context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(right: 0.0),
+          child: cancelButton(context),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 0.0),
+          child: submitButton(context),
+        ),
+      ],
+    );
+  }
+
+  Widget cancelButton(context) {
+    return ElevatedButton(
+      onPressed: () async {
+        await dropToken(context);
+        WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+          Navigator.pop(context);
+        });
+      },
+      style: ElevatedButton.styleFrom(
+        foregroundColor: Colors.black,
+        backgroundColor: Colors.white,
+        fixedSize: const Size(160.0, 40.0),
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(20.0),
+          ),
+        ),
+      ),
+      child: const Text('취소'),
+    );
+  }
+
+  Widget submitButton(context) {
+    return ElevatedButton(
+      onPressed: () async {
+        OAuthToken? token = await getToken(context);
+        http.Response response = await http.post(
+          Uri.http(backendDomain, '/api/account'),
+          headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Authorization': '${token!.toJson()}',
+          },
+          body: jsonEncode({
+            'name': registerController.text,
+          }),
+        );
+
+        if (response.statusCode == 201) {
+          WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+            Navigator.pop(context);
+          });
+        } else {
+          handleError(
+            context,
+            'Response Status Code Error.\nStatusCode: ${response.statusCode}',
+            'sign_page.dart',
+            'registerWidget',
+          );
+        }
+      },
+      style: ElevatedButton.styleFrom(
+        fixedSize: const Size(160.0, 40.0),
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            bottomRight: Radius.circular(20.0),
+          ),
+        ),
+      ),
+      child: const Text('확인'),
+    );
+  }
 }
