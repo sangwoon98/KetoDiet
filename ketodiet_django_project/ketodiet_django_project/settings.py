56b16fa66c426f2c22d8bbae3f77380aaa71c04f
sangwoon98 <dbstkddns123@naver.com>
2023-04-12 18:53:45 +0900

    4/12 19:00

--- ketodiet_django_project/ketodiet_django_project/settings.py
+++ ketodiet_django_project/ketodiet_django_project/settings.py
@@ -56,7 +56,9 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8000",
+    "http://localhost:8000",
 ]
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'ketodiet_django_project.urls'
 
