e0d8f88dd42fe48d0469f6f8d009a700ece00963
JauntySoul <joosunkim99@icloud.com>
2023-06-15 23:19:37 +0900

    2023/06/15 23:20

--- ketodiet_django_project/ketodiet_django_project/settings.py
+++ ketodiet_django_project/ketodiet_django_project/settings.py
@@ -91,24 +91,24 @@ WSGI_APPLICATION = 'ketodiet_django_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES= {
-    'default': {
-        'ENGINE':'django.db.backends.sqlite3',
-        'NAME':BASE_DIR/'db.sqlite3',
-    }
-}
-
-# DATABASES = {
+# DATABASES= {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'HOST':'db-ketodiet.cluster-ckhdgevhh7ab.ap-northeast-2.rds.amazonaws.com',
-#         'NAME': 'ketodiet_devops',
-#         'USER':'admin',
-#         'PASSWORD':'24532453',
-#         'PORT':'3306',
-#         'OPTIONS':{'charset':'utf8'},
+#         'ENGINE':'django.db.backends.sqlite3',
+#         'NAME':BASE_DIR/'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST':'db-ketodiet.cluster-ckhdgevhh7ab.ap-northeast-2.rds.amazonaws.com',
+        'NAME': 'ketodiet_devops',
+        'USER':'admin',
+        'PASSWORD':'24532453',
+        'PORT':'3306',
+        'OPTIONS':{'charset':'utf8'},
+    }
+}
 
 
 
@@ -169,3 +169,68 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'dbstkddns123456@gmail.com'  # Gmail 계정 이메일
 EMAIL_HOST_PASSWORD = 'xxjjiblbzgntqtvn'  # Gmail 계정 비밀번호
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse',
+        },
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
+        },
+    },
+    'formatters': {
+        'django.server': {
+            '()': 'django.utils.log.ServerFormatter',
+            'format': '[{server_time}] {message}',
+            'style': '{',
+        },
+        'standard': {
+            'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'INFO',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+        },
+        'django.server': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+            'formatter': 'django.server',
+        },
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        },
+        'file': {
+            'level': 'INFO',
+            'encoding': 'utf-8',
+            'filters': ['require_debug_false'],
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': BASE_DIR / 'logs/mysite.log',
+            'maxBytes': 1024*1024*5,  # 5 MB
+            'backupCount': 5,
+            'formatter': 'standard',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console', 'mail_admins', 'file'],
+            'level': 'INFO',
+        },
+        'django.server': {
+            'handlers': ['django.server'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+        'my': {
+            'handlers': ['console', 'file'],
+            'level': 'INFO',
+        },
+    }
+}
